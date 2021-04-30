b691289a5387bae25fb92c3017ac4e6cff8528d4
rejid4996 <deepak.reji@arcadis.com>
2021-04-30 10:03:55 +0530

    last

--- script.py
+++ script.py
@@ -6,3 +6,9 @@ Created on Thu Apr 29 10:41:19 2021
 """
 
 # projection
+import os
+import pandas as pd
+
+os.chdir(r'C:\Users\rejid4996\OneDrive - ARCADIS\Desktop\Projection')
+people_df = pd.read_excel('data.xlsx', sheet_name = 'Sheet1')
+
